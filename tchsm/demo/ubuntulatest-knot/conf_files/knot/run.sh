8f5cbe225b66639ba12b0bf1eb221733fdbe9593
Francisco Montoto <fmontotomonroy@gmail.com>
2016-09-06 17:48:33 -0300

    code cleaning

--- tchsm/demo/ubuntulatest-knot/conf_files/knot/run.sh
+++ tchsm/demo/ubuntulatest-knot/conf_files/knot/run.sh
@@ -4,7 +4,6 @@ set -e
 
 mkdir /root/kasp && cd /root/kasp
 
-# export KEYMGR_DIR=/root/kasp
 export TCHSM_CONFIG=/root/knot_conf/cryptoki.conf
 
-knotd -v -c /root/knot_conf/knot.conf
+knotd -c /root/knot_conf/knot.conf
