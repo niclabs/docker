82a1edb773688d47ccb63f4291c423c351417c33
javier-op <javiermoralesr95@gmail.com>
2017-10-11 10:52:34 -0300

    Cambio reports/config.py

--- adkintunmobile/server/reports/config.py
+++ adkintunmobile/server/reports/config.py
@@ -2,7 +2,7 @@ class DefaultConfig(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    BIND_KEY = "frontend"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     USER_SERVER = "this-should-be-changed"
     SECRET_KEY_SERVER = "this-should-be-changed"
@@ -10,7 +10,7 @@ class DefaultConfig(object):
     DB_NAME_SERVER = "this-should-be-changed"
     SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(USER_SERVER, SECRET_KEY_SERVER,
                                                                 HOST_SERVER, DB_NAME_SERVER)
-
+    BIND_KEY = "frontend"
     USER_FRONTEND = "this-should-be-changed"
     SECRET_KEY_FRONTEND = "this-should-be-changed"
     HOST_FRONTEND = "this-should-be-changed"
@@ -19,25 +19,34 @@ class DefaultConfig(object):
         BIND_KEY: "postgresql://{}:{}@{}/{}".format(USER_FRONTEND, SECRET_KEY_FRONTEND,
                                                     HOST_FRONTEND, DB_NAME_FRONTEND)
     }
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class TestConfig(object):
-    USER = "this-should-be-changed"
-    SECRET_KEY = "this-should-be-changed"
-    BIND_KEY = "frontend"
-    SQLALCHEMY_DATABASE_URI = "postgresql://" + USER + ":" + SECRET_KEY + "@localhost/this-should-be-changed"
-    SQLALCHEMY_BINDS = {
-        BIND_KEY: "postgresql://" + USER + ":" + SECRET_KEY + "@localhost/this-should-be-changed"
-    }
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
     TESTING = True
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+    USER_SERVER = "this-should-be-changed"
+    SECRET_KEY_SERVER = "this-should-be-changed"
+    HOST_SERVER = "this-should-be-changed"
+    DB_NAME_SERVER = "this-should-be-changed"
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(USER_SERVER, SECRET_KEY_SERVER,
+                                                                HOST_SERVER, DB_NAME_SERVER)
+    BIND_KEY = "frontend"
+    USER_FRONTEND = "this-should-be-changed"
+    SECRET_KEY_FRONTEND = "this-should-be-changed"
+    HOST_FRONTEND = "this-should-be-changed"
+    DB_NAME_FRONTEND = "this-should-be-changed"
+    SQLALCHEMY_BINDS = {
+        BIND_KEY: "postgresql://{}:{}@{}/{}".format(USER_FRONTEND, SECRET_KEY_FRONTEND,
+                                                    HOST_FRONTEND, DB_NAME_FRONTEND)
+    }
 
 
 class Files:
     STATIC_FILES_FOLDER = "app/static"
     REPORTS_FOLDER = STATIC_FILES_FOLDER + "/" + "reports"
     LOGS_FOLDER = "tmp"
-    REPORTS_LOG_FILE = "report.log"
+    REPORTS_LOG_FILE = "update.log"
+
