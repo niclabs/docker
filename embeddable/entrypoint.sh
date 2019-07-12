9b30dae3a1c1d62ad5f60ccdb1167520cc4a2acf
Felipe Lalanne <felipe@lalanne.cl>
2019-07-12 13:36:42 -0400

    Revert to 'user' as username for compatibility

--- embeddable/entrypoint.sh
+++ embeddable/entrypoint.sh
@@ -10,16 +10,16 @@ fi
 
 # Handle special flags if we're root
 if [ $(id -u) == 0 ] ; then
-    if runuser -u dev -- test ! -w /home/dev/work; then 
+    if runuser -u user -- test ! -w /home/user/work; then 
         # Fix permissions on workdir if mounted as volume
-        chgrp 100 /home/dev/work
-        chmod g+rwX /home/dev/work
-        chmod +6000 /home/dev/work
+        chgrp 100 /home/user/work
+        chmod g+rwX /home/user/work
+        chmod +6000 /home/user/work
     fi
 
-    # Exec the command as dev with the PATH and the rest of
+    # Exec the command as user with the PATH and the rest of
     # the environment preserved
-    exec runuser -p -u dev "${cmd[@]}"
+    exec runuser -p -u user "${cmd[@]}"
 else
     # Check if user has overridden the uid/gid that
     # container runs as. Check that the user has an entry in the passwd
@@ -28,8 +28,8 @@ else
     if [[ "$STATUS" != "0" ]]; then
         if [[ -w /etc/passwd ]]; then
             echo "Adding passwd file entry for $(id -u)"
-            sed -e "s/^dev:/notdev:/" /etc/passwd > /tmp/passwd
-            echo "dev:x:$(id -u):$(id -g):,,,:/home/dev:/bin/bash" >> /tmp/passwd
+            sed -e "s/^user:/notuser:/" /etc/passwd > /tmp/passwd
+            echo "user:x:$(id -u):$(id -g):,,,:/home/user:/bin/bash" >> /tmp/passwd
             cat /tmp/passwd > /etc/passwd
             rm /tmp/passwd
         else
@@ -37,16 +37,16 @@ else
         fi
     fi
     
-    if [[ ! -w /home/dev/work ]]; then 
+    if [[ ! -w /home/user/work ]]; then 
         # Fix permissions on workdir if mounted as volume
-        sudo chgrp 100 /home/dev/work
-        sudo chmod g+rwX /home/dev/work
-        sudo chmod +6000 /home/dev/work
+        sudo chgrp 100 /home/user/work
+        sudo chmod g+rwX /home/user/work
+        sudo chmod +6000 /home/user/work
     fi
 
     # Warn if the user isn't going to be able to write files to $HOME.
-    if [[ ! -w /home/dev ]]; then
-        echo 'Warning: Container must be run with group "users" to update files in /home/dev'
+    if [[ ! -w /home/user ]]; then
+        echo 'Warning: Container must be run with group "users" to update files in /home/user'
     fi
 
     # Execute the command
