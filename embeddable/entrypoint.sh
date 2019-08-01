422fa8b7939a551b296b05b3b387aa0063fee801
Felipe Lalanne <felipe@lalanne.cl>
2019-08-01 11:26:16 -0400

    Remove trailing whitespace in entrypoint.sh

--- embeddable/entrypoint.sh
+++ embeddable/entrypoint.sh
@@ -10,37 +10,15 @@ fi
 
 # Handle special flags if we're root
 if [ $(id -u) == 0 ] ; then
-    if runuser -u user -- test ! -w /home/user/work; then 
+    if runuser -u user -- test ! -w /home/user/work; then
         # Fix permissions on workdir if mounted as volume
         chgrp 100 /home/user/work
         chmod g+rwX /home/user/work
         chmod +6000 /home/user/work
     fi
 
-    # Enable IPV6
-    sysctl -w net.ipv6.conf.all.disable_ipv6=0 > /dev/null
-    
-    # Exec the command as user with the PATH and the rest of
-    # the environment preserved
-    exec runuser -p -u user "${cmd[@]}"
-else
-    # Check if user has overridden the uid/gid that
-    # container runs as. Check that the user has an entry in the passwd
-    # file and if not add an entry.
-    STATUS=0 && whoami &> /dev/null || STATUS=$? && true
-    if [[ "$STATUS" != "0" ]]; then
-        if [[ -w /etc/passwd ]]; then
-            echo "Adding passwd file entry for $(id -u)"
-            sed -e "s/^user:/notuser:/" /etc/passwd > /tmp/passwd
-            echo "user:x:$(id -u):$(id -g):,,,:/home/user:/bin/bash" >> /tmp/passwd
-            cat /tmp/passwd > /etc/passwd
-            rm /tmp/passwd
-        else
-            echo 'Warning: Container must be run with group "root" to update passwd file'
-        fi
-    fi
-    
-    if [[ ! -w /home/user/work ]]; then 
+
+    if [[ ! -w /home/user/work ]]; then
         # Fix permissions on workdir if mounted as volume
         sudo chgrp 100 /home/user/work
         sudo chmod g+rwX /home/user/work
@@ -51,7 +29,7 @@ else
     if [[ ! -w /home/user ]]; then
         echo 'Warning: Container must be run with group "users" to update files in /home/user'
     fi
-    
+
     # Enable IPV6
     sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0 > /dev/null
 
