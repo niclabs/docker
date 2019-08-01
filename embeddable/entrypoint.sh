36654dfd37e57ac96d0b58f28cbd3a0164da8771
Felipe Lalanne <felipe@lalanne.cl>
2019-08-01 13:44:24 -0400

    Revert entrypoint to commit da9f569

--- embeddable/entrypoint.sh
+++ embeddable/entrypoint.sh
@@ -17,6 +17,28 @@ if [ $(id -u) == 0 ] ; then
         chmod +6000 /home/user/work
     fi
 
+    # Enable IPV6
+    sysctl -w net.ipv6.conf.all.disable_ipv6=0 > /dev/null
+
+    # Exec the command as user with the PATH and the rest of
+    # the environment preserved
+    exec runuser -p -u user "${cmd[@]}"
+else
+    # Check if user has overridden the uid/gid that
+    # container runs as. Check that the user has an entry in the passwd
+    # file and if not add an entry.
+    STATUS=0 && whoami &> /dev/null || STATUS=$? && true
+    if [[ "$STATUS" != "0" ]]; then
+        if [[ -w /etc/passwd ]]; then
+            echo "Adding passwd file entry for $(id -u)"
+            sed -e "s/^user:/notuser:/" /etc/passwd > /tmp/passwd
+            echo "user:x:$(id -u):$(id -g):,,,:/home/user:/bin/bash" >> /tmp/passwd
+            cat /tmp/passwd > /etc/passwd
+            rm /tmp/passwd
+        else
+            echo 'Warning: Container must be run with group "root" to update passwd file'
+        fi
+    fi
 
     if [[ ! -w /home/user/work ]]; then
         # Fix permissions on workdir if mounted as volume
